bca14d3d6039feae94b1e4f49fc46d0d7ae29857
arnandofajar <arnandofajar@gmail.com>
2022-08-31 06:41:59 +0700

    get json with axios

--- src/component/Post/Post.js
+++ src/component/Post/Post.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const Post = (props) => {
   return (
-    <div className="card mt-4" style={{width: 18 + 'rem'}}>
-      <img src="https://media.istockphoto.com/photos/professional-blogger-working-on-his-new-post-for-social-media-at-home-picture-id1252684502?b=1&k=20&m=1252684502&s=170667a&w=0&h=3kK22loPkc8S4_iJdVZ4_IpoOeIKI5zVe4LbqzhD9bM=" className="card-img-top" alt="..." />
-          <div className="card-body">
-         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-         </div>
+    <div className="card mt-4" style={{ width: 18 + "rem" }}>
+      <img
+        src="https://media.istockphoto.com/photos/professional-blogger-working-on-his-new-post-for-social-media-at-home-picture-id1252684502?b=1&k=20&m=1252684502&s=170667a&w=0&h=3kK22loPkc8S4_iJdVZ4_IpoOeIKI5zVe4LbqzhD9bM="
+        className="card-img-top"
+        alt="..."
+      />
+      <div className="card-body">
+        <p className="card-text">{props.title}</p>
+        <p className="card-text">{props.desc}</p>
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Post
+export default Post;
--- src/container/BlogPost/BlogPost.js
+++ src/container/BlogPost/BlogPost.js
@@ -1,14 +1,40 @@
-import React, { Component, Fragment } from 'react'
-import Post from '../../component/Post/Post'
+import axios from "axios";
+import React, { Component, Fragment } from "react";
+import Post from "../../component/Post/Post";
 
 export class BlogPost extends Component {
+  state = {
+    post: [],
+  };
+
+  componentDidMount() {
+    // fetch("https://jsonplaceholder.typicode.com/posts")
+    //   .then((response) => response.json())
+    //   .then((json) => {
+    //     this.setState({
+    //       post: json,
+    //     });
+    //     console.log(json);
+    //   });
+
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
+      console.log(res);
+      this.setState({
+        post: res.data,
+      });
+    });
+  }
+
   render() {
     return (
-    <Fragment>
-        <Post />
-    </Fragment>
-    )
+      <Fragment>
+        <p>BlogPost</p>
+        {this.state.post.map((post) => {
+          return <Post key={post.id} title={post.title} desc={post.body} />;
+        })}
+      </Fragment>
+    );
   }
 }
 
-export default BlogPost
+export default BlogPost;
